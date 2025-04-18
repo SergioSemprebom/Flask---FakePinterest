fce2d8bf20534a35c2a577ad2465d39d89b5a083
Sergio Luiz Semprebom <sergiosemprebom@gmail.com>
2025-04-18 10:18:26 -0300

    update 18-04-25

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -9,7 +9,7 @@ from flask_bcrypt import Bcrypt # importa o Bcrypt do flask_bcrypt
 app = Flask(__name__) # cria uma instância do Flask, __name__ é o nome do módulo atual
 # o Flask vai saber onde estão os arquivos estáticos, templates e etc.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db' # configura o banco de dados, nesse caso, um sqlite
-app.config["SECRET_KEY"] = '245aec28c2caa1919373ab04652ad471' # chave secreta para proteger as sessões do Flask
+app.config["SECRET_KEY"] = '******$%#@$%TREWSDFGHthjgfrtjkiur*((64))' # chave secreta para proteger as sessões do Flask
 app.config['UPLOAD_FOLDER'] = '/static/fotos_posts' # pasta onde as fotos serão salvas
 
 database = SQLAlchemy(app) # cria uma instância do SQLAlchemy, passando o app como parâmetro
