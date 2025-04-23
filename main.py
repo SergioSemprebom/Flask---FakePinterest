93331908cffcaadde02338ecd992bf24b0bfe96d
Sergio Semprebom <sergiosemprebom@gmail.com>
2025-04-22 22:03:16 -0300

    updateFINAL-Renderizado

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from fakepinterest import app # importa o app do arquivo fakepinterest.py
 
     
 if __name__ == '__main__': # só vai executar se o arquivo for executado diretamente
-    app.run(debug=True) # vai debugar o código, se houver erro, vai mostrar no console
+    app.run(debug=False) # vai debugar o código, se houver erro, vai mostrar no console
